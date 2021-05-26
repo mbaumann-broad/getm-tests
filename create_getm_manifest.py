8ea783e3bf1189c6166ec37778b2bc962cb44b7d
Michael Baumann (Broad) <mbaumann@broadinstitute.org>
2021-05-26 15:57:28 -0700

    WIP - Initial end-to-end implementation appears to work

--- create_getm_manifest.py
+++ create_getm_manifest.py
@@ -1,30 +1,161 @@
-import sys
+#!/usr/bin/env python3
 
+import sys
+from subprocess import Popen, PIPE
 import json
 
-class SamAuth:
-    pass
+import requests
+
+
+class GoogleAuth:
+    def run_subprocess(self, cmd, debug=False) -> str:
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+
+        stdout_str = stdout.decode("utf-8").strip()
+        stderr_str = stderr.decode("utf-8").strip()
+
+        if debug:
+            print("StdOut: " + stdout_str)
+            print("StdErr: " + stderr_str)
+
+        if p.returncode != 0:
+            error_text = "ERROR: unable to call command: " + cmd + "\n\n" + stdout_str + "\n\n" + stderr_str
+            raise Exception(error_text)
+
+        return stdout_str
+
+    def get_terra_user_token(self) -> str:
+        global terra_user_token
+        if terra_user_token is None:
+            # terra_user_token = self.run_subprocess("gcloud auth print-access-token")
+            terra_user_token = self.run_subprocess("gcloud auth application-default print-access-token")
+        return terra_user_token
+
 
 class BondProxy:
-    pass
+    bond_hostname = "broad-bond-prod.appspot.com"
 
-class FenceProxy:
-    pass
+    def get_fence_token(self, bond_provider: str, terra_user_token: str) -> str:
+        headers = {
+            'authorization': f"Bearer {terra_user_token}",
+            'content-type': "application/json"
+        }
+        resp = requests.get(f"https://{self.bond_hostname}/api/link/v1/{bond_provider}/accesstoken", headers=headers)
+        print(f"Request URL: {resp.request.url}")
+        resp.raise_for_status()
+        return resp.json().get('token')
+
 
 class MockMartha:
+    def __init__(self):
+        self.drs_server_hostname = None
+        self.drs_access_type = None
+        self.bond_provider = None
+
+    def set_service_providers(self, drs_uri: str) -> None:
+        if drs_uri.startswith("drs://dg.4503"):
+            self.drs_server_hostname = "gen3.biodatacatalyst.nhlbi.nih.gov"
+            self.drs_access_type = "gs"
+            self.bond_provider = "fence"
+        else:
+            raise NotImplementedError(f"Support for this DRS URI has not yet been implemented: {drs_uri}")
+
+    def get_drs_metadata(self, drs_uri: str) -> dict:
+        assert drs_uri.startswith("drs://")
+        object_id = drs_uri.split(":")[-1]
+
+        headers = {
+            'content-type': "application/json"
+        }
+
+        resp = requests.get(f"https://{self.drs_server_hostname}/ga4gh/drs/v1/objects/{object_id}", headers=headers)
+        print(f"Request URL: {resp.request.url}")
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_drs_access_id(self, drs_metadata: dict) -> str:
+        for access_method in drs_metadata['access_methods']:
+            if access_method['type'] == self.drs_access_type:
+                return access_method['access_id']
+        return None
+
+    def get_gen3_drs_access(self, fence_user_token: str, drs_uri: str, access_id: str) -> dict:
+
+        assert drs_uri.startswith("drs://")
+        object_id = drs_uri.split(":")[-1]
+
+        headers = {
+            'authorization': f"Bearer {fence_user_token}",
+            'content-type': "application/json"
+        }
+
+        resp = requests.get(f"https://{self.drs_server_hostname}/ga4gh/drs/v1/objects/{object_id}/access/{access_id}",
+                            headers=headers)
+        print(f"Request URL: {resp.request.url}")
+        print(resp.text)
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_filename(self, url: str) -> str:
+        from pathlib import Path
+        from urllib.parse import urlparse
+
+        url_parts = urlparse(url)
+        path_parts = Path(url_parts.path)
+        return path_parts.name
+
+    def format_drs_checksums_as_object(self, drs_checksums: list) -> dict:
+        checksums_object = dict()
+        for checksum in drs_checksums:
+            checksums_object[checksum['type']] = checksum['checksum']
+        return checksums_object
+
     def resolve(self, drs_uri: str) -> dict:
-        pass
+        self.set_service_providers(drs_uri)
+        drs_metadata = self.get_drs_metadata(drs_uri)
+        drs_access_id = self.get_drs_access_id(drs_metadata)
+        fence_user_token = BondProxy().get_fence_token(self.bond_provider, GoogleAuth().get_terra_user_token())
+        drs_access_response = self.get_gen3_drs_access(fence_user_token, drs_uri, drs_access_id)
+        print(f"drs_metadata = {json.dumps(drs_metadata, indent=4)}")
+        print(f"drs_access_response = {json.dumps(drs_access_response, indent=4)}")
+        martha_response = dict(accessUrl=drs_access_response,
+                               bondProvider=self.bond_provider,
+                               bucket=None, # Not needed
+                               contentType=None, # Not needed
+                               fileName=self.get_filename(drs_access_response['url']),
+                               googleServiceAccount=None, # Not needed
+                               gsUri=None, # Not needed
+                               hashes=self.format_drs_checksums_as_object(drs_metadata['checksums']),
+                               name=drs_metadata['name'],
+                               size=drs_metadata['size'],
+                               timeCreated=drs_metadata['created_time'],
+                               timeUpdated=drs_metadata['updated_time'])
+        print(f"martha_response={json.dumps(martha_response, indent=4)}")
+        return martha_response
+
 
 class ManifestGenerator:
-    def get_providers(self, drs_uri: str) -> tuple:
-        drs_hostname = None
-        bond_provider = None
-        # TODO Implement!
-        return drs_hostname, bond_provider
 
+    def create_filepath(self, drs_uri: str, filename: str) -> str:
+        drs_uri_portion = drs_uri.replace("drs://","").replace(":","_").replace('/',"_")
+        return f"/cromwell_root/{drs_uri_portion}/{filename}"
+
+    def convert_martha_response_to_manifest_entry(self, drs_uri: str, martha_response: dict) -> dict:
+        manifest_entry = {
+            'url': martha_response['accessUrl']['url'],
+            'checksum': martha_response['hashes']['md5'],
+            'checksum-algorithm': 'md5',
+            'filepath': self.create_filepath(drs_uri, martha_response['fileName'])
+        }
+        print(f"manifest_entry={json.dumps(manifest_entry, indent=4)}")
+        return manifest_entry
 
     def resolve_to_manifest_entry(self, drs_uri: str) -> dict:
-        pass
+        martha = MockMartha()
+        martha_response = martha.resolve(drs_uri)
+        manifest_entry = self.convert_martha_response_to_manifest_entry(drs_uri, martha_response)
+        return manifest_entry
 
     def resolve_all_to_manifest_json(self, drs_uris: list) -> list:
         manifest_entries = []
@@ -33,7 +164,7 @@ class ManifestGenerator:
             manifest_entries.append(manifest_entry)
         return manifest_entries
 
-    def write_manifest(self, manifest_filename: str, manifest_content: dict) -> None:
+    def write_manifest(self, manifest_filename: str, manifest_content: list) -> None:
         with open(manifest_filename, "w") as fh:
             fh.write(json.dumps(manifest_content, indent=4))
 
@@ -41,15 +172,15 @@ class ManifestGenerator:
         manifest_content = self.resolve_all_to_manifest_json(drs_uri_list)
         self.write_manifest(manifest_filename, manifest_content)
 
-# Main
-print("Hello Getm!")
 
+# Main
 assert len(sys.argv) >= 3, "Usage: create_getm_manifest.py manifest_filename drs_uri [drs_uri ...]"
 manifest_filename = sys.argv[1]
 drs_uri_list = sys.argv[2:]
 print(f"manifest_filename={manifest_filename}")
 print(f"drs_uri_list={drs_uri_list}")
 
+terra_user_token = None
 manifest_generator = ManifestGenerator()
 manifest_generator.create_manifest(manifest_filename, drs_uri_list)
 
