dc4b895eef03dca90b62af633e4fdac189101e2d
Brian Hannafious <bhannafi@ucsc.edu>
2021-07-07 13:23:15 -0700

    review comments

--- scripts/inject_gs_credentials/inject_credentials.py
+++ scripts/inject_gs_credentials/inject_credentials.py
@@ -10,17 +10,18 @@ import WDL.Value
 
 def main(cfg, logger, run_id, run_dir, task, **recv):
     # Include GCP credentials in task input
-    p = os.path.join(os.path.expanduser("~"), ".config", "gcloud")
-    if not os.path.exists(p) or not os.path.isfile(os.path.join(p, "application_default_credentials.json")):
-        msg = ("GCP credentials are expected in `~/.config/gcloud`"
-               "Please authenticate with GCP using `gcloud auth application-default login`")
+    gcloud_config_dir = os.path.join(os.path.expanduser("~"), ".config", "gcloud")
+    if (not os.path.exists(gcloud_config_dir)
+            or not os.path.isfile(os.path.join(gcloud_config_dir, "application_default_credentials.json"))):
+        msg = ("GCP credentials are expected in '~/.config/gcloud'. "
+               "Please authenticate with GCP using 'gcloud auth application-default login'")
         raise RuntimeError(msg)
-    recv['inputs'] = recv['inputs'].bind("gcp_credentials", WDL.Value.Directory(p))
+    recv['inputs'] = recv['inputs'].bind("gcp_credentials", WDL.Value.Directory(gcloud_config_dir))
     recv = yield recv
 
     # inject credentials
     for host_path, container_path in recv['container'].input_path_map.items():
-        if p in host_path:
+        if host_path.startswith(gcloud_config_dir):
             recv['command'] = _inject_sh.format(container_path=container_path) + "\n\n" + recv["command"]
             recv = yield recv
             break
